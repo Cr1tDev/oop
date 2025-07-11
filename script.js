188831ce679124ed47dd1d1d9f1d34f06852fde6
Ivanarre <arre2397@gmail.com>
2025-07-11 21:03:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,236 @@
 'use strict';
+
+// =============CONSTRUCTOR FUNCTION & NEW OPERATOR======================
+// const Person = function (fullName, birthYear) {
+//   // Instance properties
+//   this.fullName = fullName;
+//   this.birthYear = birthYear;
+
+//   // Never do this
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
+
+// const ivan = new Person('Ivan', 2002);
+// console.log(ivan);
+
+// // 1. New {} is created
+// // 2. function is called, this = {}
+// // 3. {} linked to prototype
+// // 4 .function automatically return {}
+
+// const jonabel = new Person('Jonabel', 2001);
+// const kokey = new Person('Kokey', 1999);
+// console.log(jonabel, kokey);
+
+// console.log(ivan instanceof Person);
+
+// // Prototypes
+// console.log(Person.prototype);
+// Person.prototype.calcAge = function () {
+//   console.log(2025 - this.birthYear);
+// };
+
+// ivan.calcAge();
+// jonabel.calcAge();
+
+// console.log(ivan.__proto__);
+// console.log(ivan.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(ivan));
+// console.log(Person.prototype.isPrototypeOf(jonabel));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// // .prototypeOfLinkObject
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(ivan, jonabel);
+
+// console.log(ivan.hasOwnProperty('fullName'));
+// console.log(ivan.hasOwnProperty('species'));
+
+// // Static method
+// Person.hey = function () {
+//   console.log('Hey there!');
+//   console.log(this);
+// };
+
+// Person.hey();
+
+// //===============================================
+// console.log(ivan.__proto__);
+// // Object.prototype (top of prototype chain)
+// console.log(ivan.__proto__.__proto__);
+// console.log(ivan.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 3, 4, 7, 6, 1, 1]; // new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(h1);
+// console.dir(x => x + 1);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+// const Car = function (carName, speed) {
+//   this.carName = carName;
+//   this.carSpeed = speed;
+// };
+
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
+
+// const init = function (car) {
+//   car.prototype.accelerate = function () {
+//     this.carSpeed += 10;
+//     console.log(`${this.carName} has a speed of ${this.carSpeed} km/h`);
+//   };
+
+//   car.prototype.brake = function () {
+//     this.carSpeed -= 5;
+//     console.log(`${this.carName} has a speed of ${this.carSpeed} km/h`);
+//   };
+// };
+
+// init(Car);
+
+// car1.accelerate();
+// car1.accelerate();
+// car1.brake();
+// console.log(' ');
+// car2.accelerate();
+// car2.brake();
+
+//=====================ES6 CLASSES=======================
+// // Class expression
+// // const Person = class {};
+
+// // CLass declaration
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+
+//   // Methods will be added to .prototype property
+//   // Insttance method
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   // Set a property that already exist
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   // Static method
+//   static hey() {
+//     console.log('Hey there!');
+//     console.log(this);
+//   }
+// }
+
+// const jessica = new PersonCl('Jessica Davis', 1996);
+// console.log(jessica);
+// jessica.calcAge();
+// console.log(jessica.age);
+
+// console.log(jessica.__proto__ === PersonCl.prototype);
+
+// // PersonCl.prototype.greet = function () {
+// //   console.log(`Hey ${this.fullName}`);
+// // };
+
+// jessica.greet();
+
+// // 1. CLasses are NOT hoisted
+// // 2. Class are first-class citizens
+// // 3. Classes are executed in strict mode
+
+// const walter = new PersonCl('Walter White', 1965);
+
+// PersonCl.hey();
+
+//==================SETTERS & GETTERS========================
+
+// const account = {
+//   owner: 'jonas',
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+
+// console.log(account.latest);
+// account.latest = 50;
+// console.log(account.movements);
+
+//====================Object.create========================
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
+// steven.calcAge();
+
+// console.log(steven.__proto__ === PersonProto);
+
+// const sarah = Object.create(PersonProto);
+// sarah.init('Sarah', 1979);
+// sarah.calcAge();
+
+//=======================================================
